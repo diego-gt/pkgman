8f53bccc6d6010a639faae2b8b76ccfda697d1a5
Diego Garza <86547241+diego-gt@users.noreply.github.com>
2021-07-14 05:14:26 -0500

    Main: Testing new logger

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
-#include "pkg/PackageFileParser.hpp"
+
+#include "fmt/TimeUtils.hpp"
 #include "log/Logger.hpp"
 
 int main() {
-    std::cout << "swu version 0.0.1" << std::endl;
+    std::cout << "swu version 0.0.1" << "\n";
 
-    log::Logger::debug("test");
 
-    std::string test_pkg_file = "data/make-PKGBUILD";
-    pkg::PackageFileParser pfp (test_pkg_file, pkg::PFF_ARCH);
+	//dgt::log::Logger lg (2);
+	auto lg = dgt::log::Logger();
 
-    pfp.init();
-
-    pkg::Package make = pfp.parse();
-    make.print_details();
-
+	lg.debug(dgt::fmt::get_timestamp());
     return 0;
 }
