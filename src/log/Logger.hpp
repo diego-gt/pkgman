fff0337649a3bba3b6b221b4966b1a134dd6a3a0
Diego Garza <86547241+diego-gt@users.noreply.github.com>
2021-07-14 08:37:59 -0500

    Log: Add error, info and warning log levels

--- src/log/Logger.hpp
+++ src/log/Logger.hpp
@@ -2,12 +2,23 @@
 
 #include <string>
 
-//#include "Queue.hpp"
-
 namespace dgt::log {
+enum LogLevel {
+    Error,
+    Warning,
+    Debug,
+    Info
+};
+
 class Logger {
 public:
     Logger();
+    void error(const std::string& message);
+    void warning(const std::string& message);
     void debug(const std::string& message);
+    void info(const std::string& message);
+
+private:
+    const std::string prefix_log(LogLevel level);
 };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
 #include "fmt/TimeUtils.hpp"
+#include "fmt/Delimiter.hpp"
 #include "log/Logger.hpp"
 
 int main()
 {
-    std::cout << "swu version 0.0.1"
-              << "\n";
+    std::cout << "swu version 0.0.1" << "\n";
 
-    //dgt::log::Logger lg (2);
     auto lg = dgt::log::Logger();
 
-    lg.debug(dgt::fmt::get_timestamp());
+    lg.debug("Logging...");
+	lg.error("Logging...");
+	lg.warning("Logging...");
+	lg.info("Logging...");
+
     return 0;
 }
