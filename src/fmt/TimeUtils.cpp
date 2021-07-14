c02a628c72b53d7af703854bcb008e3fc3b40537
Diego Garza <86547241+diego-gt@users.noreply.github.com>
2021-07-14 05:08:59 -0500

    Fmt: Add library with print-related functions

---
+++ src/fmt/TimeUtils.cpp
@@ -0,0 +1,22 @@
+// On Windows, msvc complains about std::localtime being unsecure
+// disable this type of warning until a better way of handling
+// this is found.
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <chrono>
+#include <ctime>
+#include <string>
+
+#include "TimeUtils.hpp"
+
+namespace dgt::fmt {
+	std::string get_timestamp() {
+		std::time_t time_now = std::time(nullptr);
+		char result[64];
+		// TODO(dgt): review this procedure to be "more secure" for msvc
+		std::strftime(result, sizeof result, "%F %T", std::localtime(&time_now));
+		return result;
+	}
+}
